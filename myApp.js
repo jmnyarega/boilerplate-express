d27d89ef6e3bf37fd915e00399f519f40c331ed8
josiahmokob0 <jmnyarega@gmail.com>
2021-10-31 23:18:06 +0300

    - sendFile

--- myApp.js
+++ myApp.js
@@ -2,8 +2,11 @@ var express = require('express');
 var app = express();
 console.log("Hello World");
 
+const filePath = `${__dirname}/views/index.html`;
+
 app.get("/", function(req, res) {
-  res.send("Hello Express");
+  // res.send("Hello Express");
+  res.sendFile(filePath);
 });
 
 
