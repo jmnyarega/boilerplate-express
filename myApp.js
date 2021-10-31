be2fba20feb0e47de9349190b0d1055d5b14c512
josiahmokob0 <jmnyarega@gmail.com>
2021-10-31 23:28:13 +0300

    - Add static Middleware function

--- myApp.js
+++ myApp.js
@@ -2,7 +2,12 @@ var express = require('express');
 var app = express();
 console.log("Hello World");
 
+// files
 const filePath = `${__dirname}/views/index.html`;
+const staticPath = `${__dirname}/public`;
+
+// global middlewares
+app.use('/public', express.static(staticPath));
 
 app.get("/", function(req, res) {
   // res.send("Hello Express");
