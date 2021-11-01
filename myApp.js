67ce7e7d849bfb239334b2e9a9e35d6c1273d84c
josiahmokob0 <jmnyarega@gmail.com>
2021-11-01 14:26:42 +0300

    - dot env

--- myApp.js
+++ myApp.js
@@ -1,4 +1,6 @@
 var express = require('express');
+
+require('dotenv').config()
 var app = express();
 console.log("Hello World");
 
@@ -9,12 +11,20 @@ const staticPath = `${__dirname}/public`;
 // global middlewares
 app.use('/public', express.static(staticPath));
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   // res.send("Hello Express");
   res.sendFile(filePath);
 });
 
-app.get("/json", (req, res) => res.json({ message: "Hello json" }));
+app.get("/json", (req, res) => {
+  let response = "Hello json";
+
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    response = response.toUpperCase();
+  }
+
+  res.json({ message: response });
+});
 
 
 
